dd384f7a9eeddbcffe4f0c6262161abb129b21c0
Katy Muthu <katymuthu@gmail.com>
2015-10-24 08:16:13 +0000

    changed to read from index.html

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+var fs = require("fs");
+var inputdata = fs.readFileSync("index.html",'utf8');
+console.log(inputdata);
+//var buf = new Buffer(100);
+
+//buf = fs.readFileSync("index.html");
+//var inputdata = buf.toString(buf);
+//console.log(inputdata);
+
 
 app.get('/', function(request, response) {
-  response.send('Hello Anurag from Keerthi!')
-})
+  response.send(inputdata);
+});
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
+
