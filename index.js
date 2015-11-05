f72e99c56e87e827906512b5aaa9dd96126ad51e
Katy Muthu <katymuthu@gmail.com>
2015-11-05 13:54:54 +0000

    changed index.js

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
 var express = require('express');
 var app = express();
-
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
-
-var fs = require("fs");
-var inputdata = fs.readFileSync("index.html",'utf8');
-console.log(inputdata);
-//var buf = new Buffer(100);
-
-//buf = fs.readFileSync("index.html");
-//var inputdata = buf.toString(buf);
-//console.log(inputdata);
-
-
+var fs = require('fs');
+var contents = fs.readFileSync('index.html').toString('utf-8');
+app.set('port', (process.env.PORT || 8080))
+app.use(express.static(__dirname + '/public'))
+ 
 app.get('/', function(request, response) {
-  response.send(inputdata);
+  response.send(contents)
 });
-
+ 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+  console.log("Node app is running at localhost:" + app.get('port'))
 });
-
